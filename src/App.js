eb19e4414ce71c09b6537c2590130a14de638066
Sean Birmingham <seannbirmingham98@gmail.com>
2019-11-20 00:27:32 -0500

    buttons change class of the box, thus changing colors

--- src/App.js
+++ src/App.js
@@ -3,26 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [greenie, setGreenie] = useState(true);
-  const [red, setRed] = useState(false);
-  const changeColor = () => {
-    if (greenie === true) {
-      setGreenie(false);
-      setRed(true);
-    } else {
-      setGreenie(true);
-      setRed(false);
-    }
-  };
+  const [classButton, setClassButton] = useState('green-box')
+
+  const greenBox = () => setClassButton('green-box')
+  const redBox = () => setClassButton('red-box')
 
   return (
     <div className="App">
-      <div>
-        <div className="green-box">greenBox</div>
-        <button className="" onClick={changeColor}>
-          Change Color
+      <div className={`${classButton} box`}>{classButton}</div>
+      <button onClick={greenBox}>
+        Green
         </button>
-      </div>
+      <button onClick={redBox}>
+        Red
+        </button>
     </div>
   );
 }
